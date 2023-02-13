ff75d1092328ac4d2f7a669f169617399a177ff0
Teodor Patsov <104070110+thedco5@users.noreply.github.com>
2023-02-13 22:35:44 +0200

    Add files via upload

---
+++ utils.php
@@ -0,0 +1,15 @@
+<?php
+    class Utils {
+        static function user_exists(PDO $pdo, String $username) : bool  {
+            return count($pdo->query("SELECT * FROM profiles WHERE username LIKE '" . $_POST["username"] . "';")->fetchAll()) == 0 ? false : true;
+        }
+        static function get_user(PDO $pdo, String $username) : array {
+            return $pdo->query("SELECT * FROM profiles WHERE username LIKE '$username';")->fetchAll(PDO::FETCH_ASSOC);
+        }
+        static function add_user(PDO $pdo, String $username, String $password) : void {
+            $sql = "INSERT INTO profiles (username, password) VALUES ('$username', '";
+            $sql .= password_hash($password, PASSWORD_DEFAULT) . "');";
+            $pdo->query($sql);
+        }
+    }
+?>
