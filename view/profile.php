c566c052a281092477e338bb4ed2dd538d09f080
Teodor Patsov <104070110+thedco5@users.noreply.github.com>
2023-05-17 15:09:20 +0300

    Празна засега

--- view/profile.php
+++ view/profile.php
@@ -3,15 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Профил</title>
+        <title>Profile</title>
     </head>
     <body>
-        <a href="index.php">..</a> <br>
-        <br>
-        <a href="forms/signout.php">Излезте от профила си</a> <br>
-        <a href="forms/register.php">Регистрирайте ce</a> <br>
-        <a href="forms/login.php">Влезте в профила си</a> <br>
-        <br>
-        <a href="recipes/get_recipes.php">Получаване на рецепти (опит)</a> <br>
+        Profile page
     </body>
 </html>
