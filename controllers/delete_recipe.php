b91901a8f3719acde293f64f1f3aff6b61269c56
Teodor Patsov <104070110+thedco5@users.noreply.github.com>
2023-06-17 12:12:35 +0300

    Поправяне бъг изтриването на рецептите.

--- controllers/delete_recipe.php
+++ controllers/delete_recipe.php
@@ -3,6 +3,7 @@
     if (isset($_POST["id"])) {
         $sql = "DELETE FROM recipes WHERE id LIKE " . $_POST["id"] . ";";
         $pdo->query($sql);
+        $stat = unlink("../controllers/recipes/r" . $_POST["id"] . ".json");
     }
     header("Location: ../view/recipes.php");
 ?>
