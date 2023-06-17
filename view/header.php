310a1aa0d5e2d876a8f91fa485387cdfb08eb9ba
ttsvety <125125557+ttsvety@users.noreply.github.com>
2023-06-18 00:02:55 +0300

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,6 +1,6 @@
 <?php include_once "../models/lang.php"; ?>
 <header>
-	<nav class="navbar navbar-expand-sm navbar-light">
+	<nav class="navbar navbar-expand-lg navbar-light">
 		<div class="container-fluid ">
 			<a href="."> <img class="logo-img" src="../img/logo.png" alt="logo"> </a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
