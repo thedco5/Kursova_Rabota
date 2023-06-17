9743e335936f881f2b17f98ecf36ea8dcbc68bce
ttsvety <125125557+ttsvety@users.noreply.github.com>
2023-06-18 00:52:15 +0300

    Update login.php

--- view/login.php
+++ view/login.php
@@ -12,7 +12,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php echo $dict["Log in"]; ?></title>
 		<link rel="stylesheet" href="../css/login.css">
-		<link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
+		 <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- BOOTSTRAP -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -49,7 +51,6 @@
 					<input type="password" class="form-control" name="password" placeholder="<?php echo $dict["Password"]; ?>" required>
 					<span></span>
 				</div>
-				<!--<a href="forgotten_password.php"> <?php echo $dict["Forgot password?"]; ?> </a><br><br>-->
 				<input type="submit" name="submit" value="<?php echo $dict["Log in"]; ?>">
 				<div class="signup_link">
 					<?php echo $dict["Not a member yet?"]; ?> 
